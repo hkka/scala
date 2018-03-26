6b326877a501c942966c938422553659584b5fed
callafhassan@gmail.com <callafhassan@gmail.com>
2018-03-26 23:38:41 +0200

    more examples ... Scala is cool git add .!

--- src/com/hkka/scala/chapter/nine/FileMatcher.scala
+++ src/com/hkka/scala/chapter/nine/FileMatcher.scala
@@ -6,17 +6,29 @@ object FileMatcher {
     filesMatching(_.endsWith(query))
 
   def filesContaining(query: String) =
-    filesMatching(_.contains(query))
+    filesMatching(_ contains(query))
 
   def filesRegex(query: String) =
     filesMatching(_.matches(query))
 
   private def filesMatching(matcher: String => Boolean) =
-    filesHere filter(f => matcher(f.getName)) toList
+    filesHere filter(f => matcher(f.getName))
+
+  private def safeNPEGet(o: String, getter: String => String) =
+    if(o == Nil || o.isEmpty) "Null" else getter(o)
+
+  private def currySafeNPE(s: String)(getter: String => String) =
+    if(s == Nil || s.isEmpty) "Null" else getter(s)
 
   def main(args: Array[String]): Unit = {
     filesEnding(".scala") foreach println
     filesContaining("Hello") foreach println
+    val printToString = (e: String) => e.toString
+    println(safeNPEGet("", printToString(_)))
+    println(safeNPEGet("Magenta", printToString(_)))
+
+    val safeNpeResult = currySafeNPE("RICHAPI") { printToString }
+    println(safeNpeResult)
   }
 
 }
